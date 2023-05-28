a55eb330399b7c26db3aa0fe52108125c802e649
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2023-05-28 17:16:54 -0400

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by matheus maignardi on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
